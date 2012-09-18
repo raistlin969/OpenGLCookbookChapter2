add5c7e716267e3dc9710e0ba5929952da3585a1
Michael Davidson <raistlin969@yahoo.com>
2012-09-18 14:49:33 -0400

    broken atlas code

--- BaseGL/Text.cpp
+++ BaseGL/Text.cpp
@@ -4,6 +4,7 @@
 #include "Logger.h"
 #include <glm/glm.hpp>
 #include "GLSLProgram.h"
+#include <cstdlib>
 
 using glm::vec4;
 
@@ -18,26 +19,25 @@ Text::Text(GLSLProgram* p) : _program(p)
     BOOST_LOG_SEV(get_global_log(), error) << "Could not open font";
   }
 
-  FT_Set_Pixel_Sizes(_face, 0, 12);
-
+  //FT_Set_Pixel_Sizes(_face, 0, 12);
 
   glGenVertexArrays(1, &_vao);
-  //glBindVertexArray(_vao);
+  glGenBuffers(1, &_vbo);
+  glBindVertexArray(_vao);
+  _atlas = new Atlas(_face, 24, _program);
 }
 
 Text::~Text()
 {
+  delete _atlas;
 }
 
 void Text::Render(const char* text, float x, float y, float sx, float sy, vec4& color)
 {
+  _program->Use();
   const char* p;
-  FT_GlyphSlot g = _face->glyph;
-  GLuint tex;
   glBindVertexArray(_vao);
-  glActiveTexture(GL_TEXTURE0);
-  glGenTextures(1, &tex);
-  glBindTexture(GL_TEXTURE_2D, tex);
+  glBindTexture(GL_TEXTURE_2D, _atlas->tex);
   GLint loc = glGetUniformLocation(_program->Handle(), "tex");
   if(loc >= 0)
     glUniform1i(loc, 0);
@@ -45,6 +45,86 @@ void Text::Render(const char* text, float x, float y, float sx, float sy, vec4& 
     BOOST_LOG_SEV(get_global_log(), error) << "Uniform tex not found";
   _program->SetUniform("color", color);
 
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+  int text_len = strlen(text);
+  Point* coords = new Point[6 * text_len];
+  int c = 0;
+  for(p = text; *p; p++)
+  {
+    float x2 = x + _atlas->c[*p].bl * sx;
+    float y2 = -y - _atlas->c[*p].bt * sy;
+    float w = _atlas->c[*p].bw * sx;
+    float h = _atlas->c[*p].bh * sy;
+
+    x += _atlas->c[*p].ax * sx;
+    y += _atlas->c[*p].ay * sy;
+
+    if(!w || !h)
+      continue;
+
+    coords[c].x = x2; coords[c].y = -y2; coords[c].s = _atlas->c[*p].tx; coords[c++].t = 0;
+    coords[c].x = x2 + w; coords[c].y =  -y2; coords[c].s = _atlas->c[*p].tx + _atlas->c[*p].bw / w; coords[c++].t =  0;
+    coords[c].x = x2; coords[c].y = -y2 - h; coords[c].s =  _atlas->c[*p].tx; coords[c++].t = _atlas->c[*p].bh / h;
+    coords[c].x = x2 + w; coords[c].y =  -y2; coords[c].s = _atlas->c[*p].tx + _atlas->c[*p].bw / w; coords[c++].t = 0;
+    coords[c].x = x2; coords[c].y = -y2 - h; coords[c].s = _atlas->c[*p].tx, coords[c++].t = _atlas->c[*p].bh / h;
+    coords[c].x = x2 + w; coords[c].y = -y2 - h; coords[c].s = _atlas->c[*p].tx + _atlas->c[*p].bw / w; coords[c++].t = _atlas->c[*p].bh / h;
+  }
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Point) * 6 * text_len, coords, GL_DYNAMIC_DRAW);
+  
+  glDrawArrays(GL_TRIANGLES, 0, c);
+
+  glBindVertexArray(0);
+}
+
+Atlas::Atlas(FT_Face face, int height, GLSLProgram* program)
+{
+  FT_Set_Pixel_Sizes(face, 0, height);
+  FT_GlyphSlot g = face->glyph;
+
+  int roww = 0;
+  int rowh = 0;
+  w = 0;
+  h = 0;
+
+  memset(c, 0, sizeof(c));
+
+  //find min size for texture holding all visible ASCII chars
+  for(int i = 32; i < 128; ++i)
+  {
+    if(FT_Load_Char(face, i, FT_LOAD_RENDER))
+    {
+      BOOST_LOG_SEV(get_global_log(), error) << "Loading character " << char(i) << " failed";
+      continue;
+    }
+    if(roww + g->bitmap.width + 1 >= MAXWIDTH)
+    {
+      w = std::max(w, roww);
+      h += rowh;
+      roww = 0;
+      rowh = 0;
+    }
+    roww += g->bitmap.width + 1;
+    rowh = std::max(rowh, g->bitmap.rows);
+  }
+
+  w = std::max(w, roww);
+  h += rowh;
+
+  glActiveTexture(GL_TEXTURE0);
+  glGenTextures(1, &tex);
+  glBindTexture(GL_TEXTURE_2D, tex);
+  GLint loc = glGetUniformLocation(program->Handle(), "tex");
+  if(loc >= 0)
+    glUniform1i(loc, 0);
+  else
+    BOOST_LOG_SEV(get_global_log(), error) << "Uniform tex not found";
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, w, h, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -52,47 +132,41 @@ void Text::Render(const char* text, float x, float y, float sx, float sy, vec4& 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  glEnableVertexAttribArray(0);
-  //glBindBuffer(GL_ARRAY_BUFFER, _vao);
-  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
-  for(p = text; *p; p++)
+  int ox = 0;
+  int oy = 0;
+  rowh = 0;
+
+  for(int i = 32; i < 128; ++i)
   {
-    if(FT_Load_Char(_face, *p, FT_LOAD_RENDER))
+    if(FT_Load_Char(face, i, FT_LOAD_RENDER))
+    {
+      BOOST_LOG_SEV(get_global_log(), error) << "Loading character " << char(i) << " failed";
       continue;
-    //int width = NextP2(g->bitmap.width);
-    //int height = NextP2(g->bitmap.rows);
-    //GLubyte* expanded_data = new GLubyte[2 * width * height];
-    //for(int j = 0; j < height; ++j)
-    //{
-    //  for(int i = 0; i < width; ++i)
-    //  {
-    //    expanded_data[2*(i+j*width)] = expanded_data[2*(i+j*width)+1] = (i>=g->bitmap.width || j>=g->bitmap.rows) ? 0 : g->bitmap.buffer[i + g->bitmap.width*j];
-    //  }
-    //}
-    //glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, g->bitmap.width, g->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, expanded_data);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, g->bitmap.width, g->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, g->bitmap.buffer);
-    float x2 = x + g->bitmap_left * sx;
-    float y2 = -y - g->bitmap_top * sy;
-    float w = g->bitmap.width * sx;
-    float h = g->bitmap.rows * sy;
-
-    Point box[4] = {
-      {x2,     -y2,     0, 0},
-      {x2 + w, -y2,     1, 0},
-      {x2,     -y2 - h, 0, 1},
-      {x2 + w, -y2 - h, 1, 1},
-    };
-  glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Point) * 4, box, GL_DYNAMIC_DRAW);
-  
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-    x += (g->advance.x >> 6) * sx;
-    y += (g->advance.y >> 6) * sy;
+    }
+    if(ox + g->bitmap.width + 1 >= MAXWIDTH)
+    {
+      oy += rowh;
+      rowh = 0;
+      ox = 0;
+    }
+    glTexSubImage2D(GL_TEXTURE_2D, 0, ox, oy, g->bitmap.width, g->bitmap.rows, GL_RED, GL_UNSIGNED_BYTE, g->bitmap.buffer);
+    c[i].ax = g->advance.x >> 6;
+    c[i].ay = g->advance.y >> 6;
+    c[i].bw = g->bitmap.width;
+    c[i].bh = g->bitmap.rows;
+    c[i].bl = g->bitmap_left;
+    c[i].bt = g->bitmap_top;
+    c[i].tx = ox / (float)w;
+    c[i].ty = oy / (float)h;
+    
+    rowh = std::max(rowh, g->bitmap.rows);
+    ox += g->bitmap.width + 1;
   }
-//  glDisableVertexAttribArray(0);
-  glBindVertexArray(0);
+
+  BOOST_LOG_SEV(get_global_log(), info) << "Generated a " << w << " x " << h << " (" << w * h / 1024 << " kb) texture atlas";
+}
+
+Atlas::~Atlas()
+{
   glDeleteTextures(1, &tex);
 }
--- BaseGL/Text.h
+++ BaseGL/Text.h
@@ -1,8 +1,10 @@
 #pragma once
-
+#include <GL/glew.h>
 #include <ft2build.h>
 #include FT_FREETYPE_H
 #include <glm/glm.hpp>
+
+#define MAXWIDTH 1024
 
 using glm::vec4;
 
@@ -16,13 +18,36 @@ struct Point
   float t;
 };
 
+struct Atlas
+{
+  GLuint tex;
+  int w;
+  int h;
+  struct
+  {
+    float ax; //advance x
+    float ay; //advance y
+    float bw; //bitmap.width
+    float bh; //bitmap.height
+    float bl; //bitmap.left
+    float bt; //bitmap.top
+    float tx; //x offset of glyph in texture coordinates
+    float ty;
+  } c[128]; //character info
+
+  Atlas(FT_Face face, int height, GLSLProgram* program);
+  ~Atlas();
+};
+
 class Text
 {
 protected:
-  unsigned int _vao;
+  GLuint _vao;
+  GLuint _vbo;
   FT_Library _ft;
   FT_Face _face;
   GLSLProgram* _program;
+  Atlas* _atlas;
 
 public:
   Text(GLSLProgram* p);
